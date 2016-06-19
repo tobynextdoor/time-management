dd19281d2761a897025cd8ca4b7134481d7a9947
Tobias Wermuth <tobynextdoor@gmail.com>
2016-06-19 15:20:11 +0200

    Initial Commit

---
+++ Time Management/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time Management
+//
+//  Created by Tobias Wermuth on 19/06/16.
+//  Copyright © 2016 Tobias Wermuth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
